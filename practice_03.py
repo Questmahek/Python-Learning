5f3fe02a0cc41356710a7f4b4b8c7a956b641723
mahek vadhel <mahekvadher555@gmail.com>
2026-06-26 13:40:43 +0530

    Add files via upload

---
+++ practice_03.py
@@ -0,0 +1,14 @@
+#logical operators
+    
+    #not        
+a=50
+b=30
+print(not(a>b))
+   #and
+Val1= True
+Val2= True
+print("and operator:", Val1 and Val2)
+   #or
+Val1= True
+Val2= False
+print("or operator:", Val1 or Val2)
