e05331a83791da7d40c25e8fca99033dd17ed19c
mahek vadhel <mahekvadher555@gmail.com>
2026-06-27 22:01:47 +0530

    Add files via upload

---
+++ python_04.py
@@ -0,0 +1,9 @@
+  #input in python
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+marks = input("Enter your marks: ")
+
+print("welcome", name)
+print("Age:", age)  
+print("Marks:", marks)
