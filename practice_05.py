4ef45461d4cdfeec20b0680b4b2fa9029cf59f77
mahek vadhel <mahekvadher555@gmail.com>
2026-06-30 16:56:41 +0530

    Add files via upload

---
+++ practice_05.py
@@ -0,0 +1,16 @@
+ #strings#
+str1="mahek"
+str2="balubhai"
+str3="vadhel"
+print(str1,str2, str3)
+
+#length of string#
+final_str = str1 + " " + str2 + " " + str3
+print(len(final_str))
+
+#indexing#
+str="bahauddin science college"
+ch=str[0]
+print(ch)
+ch=str[5]
+print(ch)
